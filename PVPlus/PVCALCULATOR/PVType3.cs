75cd5f34a5c7ce607297fe77bef4d120ed273615
k-j-k <wjdhrks10@naver.com>
2024-01-29 10:46:37 +0900

    1.0.7 p

--- PVPlus/PVCALCULATOR/PVType3.cs
+++ PVPlus/PVCALCULATOR/PVType3.cs
@@ -18,21 +18,50 @@ namespace PVPlus.PVCALCULATOR
 
         public override double Get순보험료(int n, int m, int t, int freq)
         {
-            return c.RateSegments_급부.First()[0];
-        }
+            double NP = 0;
+            double payCnt = Get연납입횟수(freq);
 
-        public override double Get영업보험료(int n, int m, int t, int freq)
-        {
-            double 순보험료 = Get순보험료(n, m, t, freq);
-            double 영업보험료 = 순보험료 / (1 - ex.Alpha_P - ex.Beta_P - ex.Gamma - ex.Ce);
+            double NNx_납입자 = GetNNx(c.Nx_납입자, c.Dx_납입자, freq, 0, m);
+            double NNx_유지자 = GetNNx(c.Nx_유지자, c.Dx_유지자, freq, 0, m);
 
-            return 영업보험료;
+            if (freq == 99)
+            {
+                NP = (c.Mx_급부[0] - c.Mx_급부[n]) / c.Dx_납입자[0];
+            }
+            else
+            {
+                NP = (NNx_유지자 - NNx_납입자) / NNx_납입자;
+            }
+
+            return NP;
         }
 
         public override double Get준비금(int n, int m, int t, int freq)
         {
-            if (t == 0) return Get순보험료(n, m, t, freq);
-            else return 0;
+            double 순보험료 = Get순보험료(n, m, t, freq);
+            double payCnt = Get연납입횟수(freq);
+
+            double NNx_납입자 = GetNNx(c.Nx_납입자, c.Dx_납입자, freq, t, m);
+            double NNx_유지자 = GetNNx(c.Nx_유지자, c.Dx_유지자, freq, t, m);
+
+            double 분자 = 0;
+            double 분모 = 1.0;
+
+            double 분자Out = NNx_유지자 - NNx_납입자;
+            double 분자In = (m > 0 && t <= m) ? 순보험료 * NNx_납입자 : 0;
+
+            if (freq == 99)
+            {
+                분자 = 분자Out;
+                분모 = c.Dx_유지자[t];
+            }
+            else
+            {
+                분자 = payCnt * (분자Out - 분자In);
+                분모 = c.Dx_유지자[t];
+            }
+
+            return 분자 / 분모;
         }
     }
 
